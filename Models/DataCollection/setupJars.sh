037877d0f95f7ac63e283f2e9a3ab0fa1847070b
Juste Raimbault <juste.raimbault@ign.fr>
2025-09-07 17:51:19 +0200

    data collection jars

---
+++ Models/DataCollection/setupJars.sh
@@ -0,0 +1,6 @@
+rm bibliodata.jar
+rm torpool.jar
+# download versions at a5d8ebd025446e (2019/02/13)
+wget https://github.com/JusteRaimbault/BiblioData/raw/refs/heads/master/jar/bibliodata.jar
+wget https://github.com/JusteRaimbault/BiblioData/raw/refs/heads/master/jar/torpool.jar
+
