8241341ee344f96a26bc03b9a58d8954ef996ba5
Juste Raimbault <juste.raimbault@ign.fr>
2025-09-09 23:46:00 +0300

    corpus_zipf

--- Models/DataCollection/Readme.md
+++ Models/DataCollection/Readme.md
@@ -12,5 +12,23 @@
    * Compute priorities: java -jar bibliodata.jar --database --priority geotheoquantintegration 2 [NOT USEFUL]
    * export : java -jar bibliodata.jar --database --export geotheoquantintegration $EXPORTFILE [fixed export function: why was not working anymore since last Y? (urbsimdigtwins quantep)]
 
+- Zipf corpus -> get 2016 corpus from https://github.com/JusteRaimbault/MetaZipf/tree/master/Network at 1a10af3269b24
+TODO reorg repo into dedicated one
+! corr parsing erros in csv:
+   1  2289     3 a double  "5184614407784841768;1981"  ""   
+ 2  2289     4 3 columns "4 columns"                 ""   
+ 3  5947     3 a double  "14482711318377341270;2003" ""   
+ 4  5947     4 3 columns "4 columns"                 ""   
+ 5 11139     3 a double  "5945408048924492775;\""    ""   
+ 6 11139     4 3 columns "4 columns"                 ""   
+ 7 28805     3 a double  "13768771170206739465;1986" ""   
+ 8 28805     4 3 columns "4 columns"                 ""   
+ 9 28830     3 a double  "9651949334057386734;2006"  ""   
+10 28830     4 3 columns "4 columns"                 ""  
+ -> corpus pas coherent (6 remaining qd filtre sur base_corpus) -> redo collection
+
+ * java -jar bibliodata.jar --database --import ../../Data/OriginCorpuses/Zipf.csv geotheoquantintegration 2 zipf
+ * java -jar bibliodata.jar --citation --mongo geotheoquantintegration 10000
+
 
 
--- Models/KDIntegration/citation.R
+++ Models/KDIntegration/citation.R
@@ -6,6 +6,7 @@ library(glue)
 library(reshape2)
 library(ggplot2)
 library(heatmaply)
+library(readr)
 
 source(paste0(Sys.getenv('CS_HOME'),'/Organisation/Models/Utils/R/plots.R'))
 source('functions.R')
@@ -28,4 +29,15 @@ while(length(which(degree(citationcorehigher)==1))>0){citationcorehigher = induc
 export_gml(citationcorehigher,'../../Data/core_evurbth.gml')
 write.csv(data.frame(title=V(citationcorehigher)$title,id=V(citationcorehigher)$name,year=V(citationcorehigher)$year),file='../../Data/Corpuses/evurbth_core.csv',row.names = F)
 
+
+#2 - Get core for Zipf
+nodes <- read_delim('https://github.com/JusteRaimbault/MetaZipf/raw/refs/heads/master/Network/Models/CitationNetwork/res/citation.csv',delim = ";",col_names = c("title","id","year"))
+edges <- read_delim('https://github.com/JusteRaimbault/MetaZipf/raw/refs/heads/master/Network/Models/CitationNetwork/res/citation_links.csv',delim = ";",col_names = c('from','to'),col_types = "cc")
+basecorpus <- read_delim('https://github.com/JusteRaimbault/MetaZipf/raw/refs/heads/master/Network/Models/CitationNetwork/data/corpus.csv',delim=";",quote="",col_names = c("title","id"),col_types = "cc")
+basecorpus = basecorpus[!is.na(basecorpus$id),]
+#basecorpus=rbind(basecorpus,c(title="Human behavior and the principle of least effort: An introduction to human ecology",id="14883856614127053528"))
+
+edges=edges[sapply(edges$to,FUN = function(id){id%in%basecorpus$id}),] # citations to level 1
+#edges[apply(edges,MARGIN = 1,FUN = function(link){link[1]%in%basecorpus$id &&link[2]=="14883856614127053528"}),] # no cit to zipf! no need to add
+nodes=nodes[sapply(nodes$id,function(id){id%in%edges$from||id%in%edges$to}),]
 
