ee8fd26b0e8d91293b318d8e7907007af126d2b3
Juste Raimbault <juste.raimbault@ign.fr>
2025-11-04 00:07:54 +0100

    tf_idf_with_basline

--- Models/KDClassification/tfidf-logit.py
+++ Models/KDClassification/tfidf-logit.py
@@ -14,7 +14,7 @@ seed = 66
 
 label_cols = ['Empirical','Theoretical','Modelling','Methodological']
 
-df = pd.read_csv('corpus_ANNOTATED.csv')
+df = pd.read_csv('corpus_ANNOTATED_TRANSLATED.csv')
 
 for col in label_cols:
     print(col)
@@ -47,11 +47,26 @@ f1_macro_scores = []
 accuracy_scores = []
 hamming_scores = []
 
+f1_micro_zero_r = []
+f1_macro_zero_r = []
+acc_zero_r = []
+
 for fold, (train_index, test_index) in enumerate(kf.split(X, y)):
     print(f"\n--- Processing Fold {fold + 1}/{n_splits} ---")
 
     X_train, X_test = X.iloc[train_index], X.iloc[test_index]
     y_train, y_test = y[train_index], y[test_index]
+
+    # Zero-R baseline
+    y_train_tuples = [tuple(row) for row in y_train]
+    pattern_counts = pd.Series(y_train_tuples).value_counts()
+    most_frequent_pattern = np.array(pattern_counts.index[0])
+    n_test_samples = y_test.shape[0]
+    y_pred_zero_r = np.tile(most_frequent_pattern, (n_test_samples, 1))
+    acc_zero_r.append(accuracy_score(y_test, y_pred_zero_r))
+    f1_micro_zero_r.append(f1_score(y_test, y_pred_zero_r, average='micro', zero_division=0))
+    f1_macro_zero_r.append(f1_score(y_test, y_pred_zero_r, average='macro', zero_division=0))
+
 
     pipeline.fit(X_train, y_train)
 
@@ -68,6 +83,9 @@ for fold, (train_index, test_index) in enumerate(kf.split(X, y)):
 
     #print(classification_report(y_test, y_pred, target_names=label_cols, zero_division=0))
 
+print(f"  Zero-R Accuracy: {np.mean(acc_zero_r):.4f} (+/- {np.std(acc_zero_r):.4f})")
+print(f"  Zero-R Micro-F1 Score:     {np.mean(f1_micro_zero_r):.4f} (+/- {np.std(f1_micro_zero_r):.4f})")
+print(f"  Zero-R Macro-F1 Score:     {np.mean(f1_macro_zero_r):.4f} (+/- {np.std(f1_macro_zero_r):.4f})")
 
 print(f"  Average Exact Match Accuracy: {np.mean(accuracy_scores):.4f} (+/- {np.std(accuracy_scores):.4f})")
 print(f"  Average Micro-F1 Score:     {np.mean(f1_micro_scores):.4f} (+/- {np.std(f1_micro_scores):.4f})")
